1549c5638fd662a86a8e7da1c5299249e0d21327
jelledebock <debockjelle@hotmail.com>
2017-03-01 20:40:22 +0100

    Implementation mergesort

--- labo2/src/main.cpp
+++ labo2/src/main.cpp
@@ -2,6 +2,8 @@
 #include "sortvector.h"
 #include "sorteermethode.h"
 #include "Nstring.h"
+#include "Mergesort.h"
+
 using namespace std;
 
 int main()
@@ -24,6 +26,13 @@ int main()
 
     STLSort<Nstring> stlSortString;
     stlSortString.meet(10,1000000, cout);
+
+    Sortvector<int> random(10);
+    random.vul_random_zonder_dubbels();
+    cout<<"Voor: "<<random<<endl;
+    Mergesort<int> mergesort;
+    mergesort(random);
+    cout<<"Na: "<<random<<endl;
 
     return 0;
 }
